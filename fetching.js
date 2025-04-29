49393eb82e7d3bac98493a0055115d270581b824
Federico <brunetti.federico.13@gmail.com>
2025-04-29 17:29:56 +0200

    lavorando sull'estetica

--- fetching.js
+++ fetching.js
@@ -12,17 +12,18 @@ const fetchBook = () => {
       const row = document.querySelector(".row");
       booksObj.forEach((book) => {
         console.log(book.title);
+        row.className = "row row-gap-3";
         const col = document.createElement("div");
-        col.className = "col-12 col-sm-6 col-md-4 col-lg-3";
+        col.className = "col-12 col-sm-6 col-md-3 col-lg-2";
 
         col.innerHTML = `
-            <div class="card">
+            <div class="card  data-bs-theme="dark">
             <img src="${book.img}" class="card-img-top cover-img" alt="book image" />
-            <div class="card-body">
+            <div class="card-body bg-light-subtle">
               <h5 class="card-title text-truncate"> ${book.title}</h5>
-              <p class="card-text"> Price: ${book.price} $</p>
-              <a href="#" class="btn btn-primary">Info </a>
-              <a href="#" class="btn btn-primary"> Add to Cart </a> 
+              <p class="card-text">${book.price} $</p>
+              <a href="#" class="btn bg-primary-subtle btn-sm mb-2">Info </a>
+              <a href="#" class="btn  btn-info btn-sm"> Add to Cart </a> 
               </div>
               </div>
         `;
